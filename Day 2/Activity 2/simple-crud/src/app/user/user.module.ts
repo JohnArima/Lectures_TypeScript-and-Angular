652c713c9abd583326f2515423389c6e022b6299
JohnArima <john.salangsang20@gmail.com>
2022-08-10 00:00:03 +0800

    Activity 2 Commit

--- Day 2/Activity 2/simple-crud/src/app/user/user.module.ts
+++ Day 2/Activity 2/simple-crud/src/app/user/user.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { FormComponent } from './components/form/form.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ProfileComponent,
+    FormComponent
+  ],
   imports: [
     CommonModule
   ]
